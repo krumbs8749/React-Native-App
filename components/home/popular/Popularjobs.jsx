4f6ca62faa5a4f6a9c2141cd1ab5943dff5be95a
ikram <krumbs8749@gmail.com>
2023-09-22 13:21:29 +0100

    feat: added api call to demo server

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,11 +4,18 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hooks/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const loading = false;
-  const error = false;
+
+  const { data, loading, error } = useFetch(
+    'search',
+    {
+      query: 'Java Developer',
+      num_pages: 1
+    }
+  );
 
   return (
     <View style={styles.container}>
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,8 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = () => {
-    const [data, setData] = useState([]);
+const key = process.env.RAPID_API_KEY;
+
+export default useFetch = (endpoint, query) => {
+    const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const options = {
+    method: 'POST',
+    url: `http://192.168.0.55:8080/api/v1/job/search`,
+    data: query,
+    headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.request(options);
+            await response;
+            setData(response.data);
+            console.log(response.data);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+            alert('There was an error fetching data', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(data) return;
+        fetchData();
+    }, [data]);
+
+    const refetch = () => {
+        setLoading(true);
+        fetchData();
+    }
+
+    return {data, loading, error, refetch};
+    
 }
